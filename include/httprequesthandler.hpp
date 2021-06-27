9d0e77e09c740458df2e55052b20fea00c9db24d
olabijed@gmail.com <olabijed@gmail.com>
2021-06-27 22:57:20 +0100

    Http request handler creatd

---
+++ include/httprequesthandler.hpp
@@ -0,0 +1,14 @@
+#ifndef QTODO_HTTPREQUESTHANDLER_HPP
+#define QTODO_HTTPREQUESTHANDLER_HPP
+
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
+
+class HttpRequestHandler
+{
+public:
+    static const QString API_URL;
+};
+
+#endif //QTODO_HTTPREQUESTHANDLER_HPP
---
+++ src/httprequesthandler.cpp
@@ -0,0 +1,4 @@
+#include "httprequesthandler.hpp"
+
+const QString HttpRequestHandler::API_URL = "http://localhost:8000/api/";
+
